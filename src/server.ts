5d55928efaab3c3b3854fe84f347817f243e7fa2
Andre Assef <andreassef02@gmail.com>
2021-08-25 22:24:29 -0300

    [UPDATE] - Iniciando rabbitmq

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from "express";
+import { RabbitInit } from "./rabbitmq";
+import { UserProducer } from "./producer";
+import { Consumer } from "./consumer";
 
 const app = express();
 
@@ -8,6 +11,19 @@ app.get("/", (request: Request, response: Response) => {
 
     return response.status(200).json({message: "Hello World!"});
 });
+
+(
+    async function() {
+        const rabbit = new RabbitInit();
+        await rabbit.execute();
+
+        const userProducer = new UserProducer();
+        await userProducer.execute();
+
+        const consume = new Consumer();
+        await consume.execute();
+    }
+)();
 
 app.listen(3333, () => {
     console.log("Server run on port 3333!");
